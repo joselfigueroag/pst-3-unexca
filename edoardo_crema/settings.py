5c1b6f86ea64c17ff8977aa5c24d6f6ba701de55
Cesar Avila <cesar.e.avila.b@gmail.com>
2023-07-06 10:49:53 -0400

    hola mundo en common

--- edoardo_crema/settings.py
+++ edoardo_crema/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "edoardo_crema.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
