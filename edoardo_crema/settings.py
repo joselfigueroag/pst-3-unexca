e5a5569c4d5c2ebd5a5bcddf31b488264b1d2f54
root <root@Eduardo-Cremas>
2023-10-25 20:49:08 +0000

    Se agrego el host pst3.solutecvzla.com en el settings.py liena allowed_host'

--- edoardo_crema/settings.py
+++ edoardo_crema/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-35en3_w5yh$kfc%0gi%v1ie&zzcoqw65@ph-g^5%a=py)2clc7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pst3.solutecvzla.com']
 
 
 # Application definition
